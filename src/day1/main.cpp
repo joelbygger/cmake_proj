4f842e43db39c2d0bc377abcee2fa4dedd0735d7
Joel Säll <joel.sael@gmail.com>
2019-03-12 19:51:28 +0100

    Code changed acc. to new style, also made manager.cpp namnespace anonymous.

--- src/day1/main.cpp
+++ src/day1/main.cpp
@@ -11,15 +11,13 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         std::cout
             << "This program can only be run with a file name as an input. File is expected to contains ASCII chars only, representing positive and negative "
                "integers, one number on each line.\n"
             << argc << "\n";
     }
-    else
-    {
+    else {
         std::cout << "Received path: " << argv[1] << "\n";
 
         manager::manager(argv[1]);
--- src/day1/manager.cpp
+++ src/day1/manager.cpp
@@ -13,56 +13,46 @@
 
 namespace fs = std::filesystem;
 
-namespace file
+namespace
 {
-    bool isValidFile(const char* path);
-} // namespace file
+    bool isValidFile(const char* path)
+    {
+        fs::path filepath{ path };
 
-bool file::isValidFile(const char* path)
-{
-    fs::path filepath{ path };
-    /* First make sure supplied path exists and is of a type we want. */
-    if (!fs::is_regular_file(filepath))
-    {
-        if (!fs::exists(filepath))
-        {
-            std::cout << "Supplied path/ file does not exist.\n";
+        /* First make sure supplied path exists and is of a type we want. */
+        if (!fs::is_regular_file(filepath)) {
+            if (!fs::exists(filepath)) {
+                std::cout << "Supplied path/ file does not exist.\n";
+            }
+            else {
+                std::cout << "Supplied path exists but is not a regular file (it's a special file).\n";
+            }
         }
-        else
-        {
-            std::cout << "Supplied path exists but is not a regular file (it's a special file).\n";
+        else if (fs::is_directory(filepath)) {
+            std::cout << "Supplied path is a directory.\n";
         }
-    }
-    else if (fs::is_directory(filepath))
-    {
-        std::cout << "Supplied path is a directory.\n";
-    }
-    else if (fs::is_character_file(filepath))
-    {
-        std::cout << "Supplied path is a character special file.\n";
-    }
-    else
-    {
-        std::cout << "This seems to a an ok file.\n";
-        return true;
-    }
+        else if (fs::is_character_file(filepath)) {
+            std::cout << "Supplied path is a character special file.\n";
+        }
+        else {
+            std::cout << "This seems to a an ok file.\n";
+            return true;
+        }
 
-    return false;
+        return false;
+    }
 }
 
 void manager::manager(char const* path)
 {
-    if (file::isValidFile(path))
-    {
+    if (file::isValidFile(path)) {
         std::ifstream ifs(path, std::ios::in);
-        if (!ifs)
-        {
+        if (!ifs) {
             throw std::runtime_error(std::string("Failed to open file: ") + std::strerror(errno));
         }
 
         int sum = 0;
-        while (!ifs.eof())
-        {
+        while (!ifs.eof()) {
             std::string txt;
             std::getline(ifs, txt);
             sum += std::stoi(txt);
