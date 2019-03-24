e88ff0201d17ee15ebf3021325a9c252850236c7
Joel Säll <joel.sael@gmail.com>
2019-03-24 22:40:25 +0100

    Solution to day 5 par 1.

--- src/day5/manager.cpp
+++ src/day5/manager.cpp
@@ -3,25 +3,46 @@
 //
 
 #include "manager.hpp"
+#include <algorithm>
+#include <cctype>
 #include <cerrno>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
 void manager::manager(char const* path)
 {
-    std::ifstream ifs(path, std::ios_base::in);   
-    if(!ifs) {
+    std::ifstream ifs(path, std::ios_base::in);
+    if (!ifs) {
         throw std::runtime_error(std::string("Failed to open file: ") + std::strerror(errno));
     }
 
-    std::vector<char> input;
-    //ifs.get(input, ifs.gcount());
+    std::vector<char> polymer;
+
+    // Read the file.
     char c;
-    while(ifs.get(c)) {
-        input.emplace_back(c);
+    while (ifs.get(c)) {
+        polymer.emplace_back(c);
     }
+    std::cout << "polymer.size(): " << polymer.size() << "\n";
+
+    // Returns true if same char but different capitalization.
+    auto compare = [](const char& x, const char& y) -> bool { return (std::toupper(x) == std::toupper(y) && x != y); };
+    while (true) {
+        auto it = std::adjacent_find(polymer.begin(), polymer.end(), compare);
+        if (it != polymer.end()) {
+            auto it2 = it;
+            std::advance(it2, 2);
+            polymer.erase(it, it2);
+        }
+        else {
+            break;
+        }
+    }
+
+    std::cout << "polymer.size(): " << polymer.size() << "\n";
 }
--- src/day5/manager.hpp
+++ src/day5/manager.hpp
@@ -5,10 +5,9 @@
 #ifndef ADVENTOFCODE2018_MANAGER_HPP
 #define ADVENTOFCODE2018_MANAGER_HPP
 
-
-namespace manager {
+namespace manager
+{
     void manager(char const* path);
 } // namespace manager
 
-
-#endif //ADVENTOFCODE2018_MANAGER_HPP
+#endif // ADVENTOFCODE2018_MANAGER_HPP
