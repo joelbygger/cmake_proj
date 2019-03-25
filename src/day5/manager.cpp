068a3d887db68241ee85db1bfc5df5c166025941
Joel Säll <joel.sael@gmail.com>
2019-03-25 22:18:14 +0100

    Refactoring, no funtional change.

--- src/day5/manager.cpp
+++ src/day5/manager.cpp
@@ -14,48 +14,76 @@
 #include <string>
 #include <vector>
 
-void manager::manager(char const* path)
+namespace
 {
-    std::ifstream ifs(path, std::ios_base::in);
-    if (!ifs) {
-        throw std::runtime_error(std::string("Failed to open file: ") + std::strerror(errno));
+    /**
+     * Reads from file.
+     * @param path PAth to file.
+     * @return File data to be used.
+     */
+    std::vector<char> getInput(const char* path)
+    {
+        std::ifstream ifs(path, std::ios_base::in);
+        if (!ifs) {
+            throw std::runtime_error(std::string("Failed to open file: ") + std::strerror(errno));
+        }
+
+        std::vector<char> polymer;
+
+        // Read the file.
+        char c;
+        while (ifs.get(c)) {
+            polymer.emplace_back(c);
+        }
+
+        return polymer;
     }
 
+    /**
+     * Remove all adjacent duplicates: same letter but different capitilization.
+     * @param polymer A lot of chars...
+     */
+    void reducePolymer(std::vector<char>& polymer)
+    {
+        // Returns true if same char but different capitalization.
+        auto compare = [](const char& x, const char& y) -> bool { return (toupper(x) == toupper(y) && x != y); };
+
+        auto searchIt = polymer.begin();
+        while (true) {
+            auto findIt = adjacent_find(searchIt, polymer.end(), compare);
+            if (findIt == polymer.end()) {
+                if (searchIt != polymer.begin()) {
+                    // Found nothing, restart search, we might not have searched form begining.
+                    searchIt = polymer.begin();
+                }
+                else {
+                    break; // Didn't find anything, we're done.
+                }
+            }
+            else {
+                auto it2 = findIt;
+                advance(it2, 2);
+
+                searchIt = polymer.erase(findIt, it2);
+
+                if (searchIt == polymer.end()) {
+                    // We erased last element, restart search from beginning.
+                    searchIt = polymer.begin();
+                }
+            }
+        }
+    }
+} // namespace
+
+void manager::manager(char const* path)
+{
     std::vector<char> polymer;
 
     // Read the file.
-    char c;
-    while (ifs.get(c)) {
-        polymer.emplace_back(c);
-    }
+    polymer = getInput(path);
+
     std::cout << "polymer.size(): " << polymer.size() << "\n";
-
-    // Returns true if same char but different capitalization.
-    auto compare = [](const char& x, const char& y) -> bool { return (std::toupper(x) == std::toupper(y) && x != y); };
-    auto searchIt = polymer.begin();
-    while (true) {
-        auto findIt = std::adjacent_find(searchIt, polymer.end(), compare);
-        if (findIt == polymer.end()) {
-            if (searchIt != polymer.begin()) {
-                // Found nothing, restart search, we might not have searched form begining.
-                searchIt = polymer.begin();
-            }
-            else {
-                break; // Didn't find anything, we're done.
-            }
-        }
-        else {
-            auto it2 = findIt;
-            std::advance(it2, 2);
-
-            searchIt = polymer.erase(findIt, it2);
-
-            if (searchIt == polymer.end()) {
-                // We erased last element, restart search from beginning.
-                searchIt = polymer.begin();
-            }
-        }
-    }
+    reducePolymer(polymer);
 
     std::cout << "polymer.size(): " << polymer.size() << "\n";
 }
