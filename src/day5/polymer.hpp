1d76cc6408a7cf73d83a7f4c208e4439c983c193
Joel Säll <joel.sael@gmail.com>
2019-03-30 23:45:41 +0100

    Made day5 more testable and added more tests.

--- src/day5/polymer.hpp
+++ src/day5/polymer.hpp
@@ -5,25 +5,18 @@
 #ifndef ADVENTOFCODE2018_POLYMER_HPP
 #define ADVENTOFCODE2018_POLYMER_HPP
 
-#include <cstddef>
 #include <tuple>
 #include <vector>
 
 namespace polymer
 {
     /**
-     * Performs a recuction, removing adjacent identical chars if one is capital and the other is not.
-     * @param polymer A whole lot of chars.
-     * @return The size of a reducuction.
-     */
-    size_t reduce(std::vector<char> polymer);
-
-    /**
      * Entry point, accepts a file, reads it and works the data.
-     * @param path to a file.
-     * @return First the reduced poölymer in the file, second the smallest reduced polymer if all occurences of a char is removed. Which char to remove is evaluated.
+     * @param polymer A whoe lot of chars.
+     * @return First the reduced poölymer in the file, second the smallest reduced polymer if all occurences of a char is removed. Which char to remove is
+     * evaluated.
      */
-    std::tuple<int, int> manager(char const* path);
+    std::tuple<int, int> manager(std::vector<char>& polymer);
 } // namespace polymer
 
 #endif // ADVENTOFCODE2018_POLYMER_HPP
