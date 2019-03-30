d558b6240794583de3d415d0c72a00dea18e0bf5
Joel Säll <joel.sael@gmail.com>
2019-03-30 10:42:21 +0100

    Refactiring in CMake world, make stuff more readable.

--- cmake/target_factory_utest.cmake
+++ cmake/target_factory_utest.cmake
@@ -1,16 +1,7 @@
-# Adds a target to run tests with Catch unit test framework.
+# Adds a target to run unit tests, currently with Catch unit test framework.
 
 # Activate CTest.
 enable_testing()
-
-# Prepare "Catch" library for other executables.
-set(CATCH_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR})
-add_library(Catch INTERFACE) # Not using the factory and not getting all compiler flags etc., an issue?
-target_include_directories(Catch INTERFACE ${CATCH_INCLUDE_DIR})
-
-# Create path to the special file with the define that will create a main(), required by Catch.
-set(CATCH_MAIN_DEFINE_FILE ${CMAKE_CURRENT_LIST_DIR}/catchMainDefine.cpp)
-
 
 
 ###
@@ -34,11 +25,12 @@ function(new_cpp_utest_executeable)
         message(WARNING "---- Unparsed arguments: " ${EXEC_UNPARSED_ARGUMENTS} " when trying to make target:" ${EXEC_NAME})
     endif()
 
+    # Use existing factory to make sure all compiler warnings etc. are included.
     new_cpp_executeable(
             NAME ${EXEC_NAME}
-            SOURCES ${EXEC_SOURCES} ${CATCH_MAIN_DEFINE_FILE} # Add an extra file with the required Catch define.
+            SOURCES ${EXEC_SOURCES}
             INCLUDE_PRIVATE ${EXEC_INCLUDE_PRIVATE}
-            LINK_PRIVATE ${EXEC_LINK_PRIVATE} Catch) # Add the Catch library.
+            LINK_PRIVATE ${EXEC_LINK_PRIVATE} Catch) # Here we add Catch library, if another utest framework is desired, change this.
 
     # Report executable to CTest.
     add_test(
