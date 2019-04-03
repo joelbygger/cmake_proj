eec8365361daf2e81cf3dc20763cdd2cf5807d49
Joel Säll <joel.sael@gmail.com>
2019-04-03 21:48:57 +0200

    Corrected code style, according to me.now code is not eact copies from the online source, but I accept.

--- testsOfFramework/libstdcxx_check/app1.cpp
+++ testsOfFramework/libstdcxx_check/app1.cpp
@@ -13,6 +13,7 @@ int main()
     std::list<int> list1 = { 1, 2, 3, 4 };
     std::list<int> list2 = { 5, 3, 4, 2 };
     list1.merge(list2);
-    for (auto x : list1)
+    for (auto x : list1) {
         std::cout << x << '\n';
+    }
 }
--- testsOfFramework/libstdcxx_check/app2.cpp
+++ testsOfFramework/libstdcxx_check/app2.cpp
@@ -12,6 +12,7 @@ int main()
 {
     std::list<int> list1 = { 1, 2, 3, 4 };
     list1.insert(list1.begin(), list1.begin(), list1.end());
-    for (auto x : list1)
+    for (auto x : list1) {
         std::cout << x << '\n';
+    }
 }
