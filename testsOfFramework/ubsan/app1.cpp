14e20113c981972494ee084e53e39ec07c131f77
Joel Säll <joel.sael@gmail.com>
2019-04-04 20:16:55 +0200

    Added new test for UBSAN.

---
+++ testsOfFramework/ubsan/app1.cpp
@@ -0,0 +1,19 @@
+/**
+ * This program intends to test that an UBSAN check will crash this application when run.
+ *
+ * Program taken from: https://developer.apple.com/documentation/code_diagnostics/undefined_behavior_sanitizer/division_by_zero
+ * Adapted original program:
+ * - to use floats (GCC flag -fsanitize=float-divide-by-zero) and 
+ * - to fool Clangs compilation checks / Clang tidy
+ */
+
+int main()
+{    
+    float sum = 10;
+    for (int i = 64; i >= 0; --i) {
+        sum /= static_cast<float>(i); // Error: division by zero on the last iteration.
+    }
+
+    return 0;
+}
+
